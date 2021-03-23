3b2ce08329558e067f4a269d08341889b9ca3081
Nazhan Harzula <47816846+nazhanHarzula@users.noreply.github.com>
2021-03-23 18:27:24 +0800

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'php-ai/php-ml' => 
+    array (
+      'pretty_version' => '0.9.0',
+      'version' => '0.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fd8b70629e16ed67307fe150ba25738449b9a6b2',
+    ),
+  ),
+);
